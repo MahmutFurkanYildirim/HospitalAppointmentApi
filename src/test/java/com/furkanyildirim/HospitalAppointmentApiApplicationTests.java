eb5f179a7877430b89f28421c70815dadd37bc5f
Furkan YILDIRIM <46926199+Furkany99@users.noreply.github.com>
2025-02-02 15:22:11 +0300

    config

---
+++ src/test/java/com/furkanyildirim/HospitalAppointmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furkanyildirim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalAppointmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
