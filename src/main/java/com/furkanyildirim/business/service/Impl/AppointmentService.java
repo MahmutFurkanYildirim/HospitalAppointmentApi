922f2f481f200b928e87426d3862c5668836b3c9
Furkan YILDIRIM <46926199+Furkany99@users.noreply.github.com>
2025-02-22 18:20:56 +0300

    Service classes

--- src/main/java/com/furkanyildirim/business/service/Impl/AppointmentService.java
+++ src/main/java/com/furkanyildirim/business/service/Impl/AppointmentService.java
@@ -1,36 +1,98 @@
 package com.furkanyildirim.business.service.Impl;
 
+import com.furkanyildirim.business.dto.AppointmentDTO;
+import com.furkanyildirim.business.service.IAppointmentService;
 import com.furkanyildirim.data.entity.Appointment;
 import com.furkanyildirim.data.entity.Doctor;
 import com.furkanyildirim.data.entity.Patient;
 import com.furkanyildirim.data.repository.AppointmentRepository;
 import com.furkanyildirim.data.repository.DoctorRepository;
 import com.furkanyildirim.data.repository.PatientRepository;
+import com.furkanyildirim.role.EStatus;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
+import java.util.List;
+
+//@RequiredArgsConstructor
 
 @Service
-public class AppointmentService {
+@Component("appointmentService")
+public class AppointmentService implements IAppointmentService<AppointmentDTO,Appointment> {
+
     private final AppointmentRepository appointmentRepository;
     private final PatientRepository patientRepository;
     private final DoctorRepository doctorRepository;
+    private final ModelMapper modelMapper;
 
-    public AppointmentService(AppointmentRepository appointmentRepository, PatientRepository patientRepository, DoctorRepository doctorRepository) {
+    @Autowired
+    public AppointmentService(AppointmentRepository appointmentRepository,PatientRepository patientRepository,DoctorRepository doctorRepository,ModelMapper modelMapper) {
         this.appointmentRepository = appointmentRepository;
         this.patientRepository = patientRepository;
         this.doctorRepository = doctorRepository;
+        this.modelMapper = modelMapper;
     }
 
-    public Appointment createAppointment(Long patientId, Long doctorId, LocalDateTime appointmentDate) {
-        Patient patient = patientRepository.findById(patientId).orElseThrow(() -> new RuntimeException("Patient not found"));
-        Doctor doctor = doctorRepository.findById(doctorId).orElseThrow(() -> new RuntimeException("Doctor not found"));
+    public AppointmentDTO entityToDto(Appointment appointment) {
+        return modelMapper.map(appointment, AppointmentDTO.class);
+    }
 
-        Appointment appointment = new Appointment();
+    public Appointment dtoToEntity(AppointmentDTO appointmentDTO) {
+        return modelMapper.map(appointmentDTO, Appointment.class);
+    }
+
+
+    @Override
+    public AppointmentDTO createAppointment(AppointmentDTO appointmentDTO) {
+        Patient patient = patientRepository.findById(appointmentDTO.getPatientId())
+                .orElseThrow(() -> new RuntimeException("Hasta bulunamadı"));
+
+        Doctor doctor = doctorRepository.findById(appointmentDTO.getDoctorId())
+                .orElseThrow(() -> new RuntimeException("Doktor bulunamadı"));
+
+        Appointment appointment = dtoToEntity(appointmentDTO);
         appointment.setPatient(patient);
         appointment.setDoctor(doctor);
-        appointment.setAppointmentDate(appointmentDate);
 
-        return appointmentRepository.save(appointment);
+        Appointment savedAppointment = appointmentRepository.save(appointment);
+
+        return entityToDto(savedAppointment);
+    }
+
+    @Override
+    public Page<AppointmentDTO> getAllAppointments(Pageable pageable) {
+        Page<Appointment> appointmentPage = appointmentRepository.findAll(pageable);
+        Page<AppointmentDTO> appointmentDTOPage = appointmentPage.map(this::entityToDto);
+        return appointmentDTOPage;
+    }
+
+    @Override
+    public AppointmentDTO getAppointmentById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<AppointmentDTO> getAppointmentByStatus(EStatus status) {
+        return List.of();
+    }
+
+    @Override
+    public AppointmentDTO updateAppointment(Long id, AppointmentDTO appointmentDTO) {
+        return null;
+    }
+
+    @Override
+    public void deleteAppointment(Long id) {
+
+    }
+
+    @Override
+    public void deleteAllAppointment() {
+
     }
 }
--- src/main/java/com/furkanyildirim/business/service/Impl/PatientService.java
+++ src/main/java/com/furkanyildirim/business/service/Impl/PatientService.java
@@ -1,28 +1,83 @@
 package com.furkanyildirim.business.service.Impl;
 
+import com.furkanyildirim.business.dto.PatientDTO;
+import com.furkanyildirim.business.service.IPatientService;
 import com.furkanyildirim.data.entity.Patient;
 import com.furkanyildirim.data.repository.PatientRepository;
+//import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+
+//@RequiredArgsConstructor
 
 @Service
-public class PatientService {
+@Component("patientService")
+public class PatientService implements IPatientService<PatientDTO,Patient> {
+
     private final PatientRepository patientRepository;
+    private final ModelMapper modelMapper;
 
-    public PatientService(PatientRepository patientRepository) {
+    public PatientService(PatientRepository patientRepository,ModelMapper modelMapper) {
         this.patientRepository = patientRepository;
+        this.modelMapper = modelMapper;
     }
 
-    public List<Patient> getAllPatients() {
-        return patientRepository.findAll();
+    public PatientDTO entityToDto(Patient patient) {
+        return modelMapper.map(patient, PatientDTO.class);
     }
 
-    public Patient getPatientById(Long id) {
-        return patientRepository.findById(id).orElseThrow(() -> new RuntimeException("Patient not found"));
+    public Patient dtoToEntity(PatientDTO patientDTO) {
+        return modelMapper.map(patientDTO, Patient.class);
     }
 
-    public Patient createPatient(Patient patient) {
-        return patientRepository.save(patient);
+    @Override
+    public PatientDTO createPatient(PatientDTO patientDTO) {
+        Patient patient = dtoToEntity(patientDTO);
+        Patient savedPatient = patientRepository.save(patient);
+        return entityToDto(savedPatient);
     }
+
+    @Override
+    public Page<PatientDTO> getAllPatients(Pageable pageable) {
+
+        Page<Patient> patientPage = patientRepository.findAll(pageable);
+        Page<PatientDTO> patientDTOPage = patientPage.map(this::entityToDto);
+
+        return patientDTOPage;
+    }
+
+    @Override
+    public PatientDTO getPatientById(Long id) {
+        Patient patient = patientRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Patient not found"));
+        return entityToDto(patient);
+    }
+
+    @Override
+    public PatientDTO updatePatient(Long id, PatientDTO patientDTO) {
+        Patient patient = patientRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Patient not found"));
+        patient.setName(patientDTO.getName());
+        patient.setEmail(patientDTO.getEmail());
+        Patient updatedPatient = patientRepository.save(patient);
+        return entityToDto(updatedPatient);
+    }
+
+    @Override
+    public void deletePatient(Long id) {
+        Patient patient = patientRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Patient not found"));
+        patientRepository.delete(patient);
+    }
+
+    @Override
+    public void deleteAllPatients() {
+        patientRepository.deleteAll();
+    }
+
+
 }
